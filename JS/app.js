e7788890d162e6e47762ef5da283dd1b92acf297
nazmia-14 <nazmihibreash@gmail.com>
2021-04-17 15:25:42 +0300

    style reservation and edit js

--- JS/app.js
+++ JS/app.js
@@ -15,12 +15,13 @@
 // }
 
 
-/
+
 let locations = [
   {
     locationName: 'Amman',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quisquam molestias, iusto facilis cum quidem ad in maxime minima, eveniet esse hic. Sunt facere minus dolor ipsa fuga nisi ex. ',
     locationImg: './img/locations/2.jpg',
+    locationImg: './img/locations/2.jpg ',
     price: 100,
     category: 'city',
     // visitors: localStorage
@@ -77,26 +78,37 @@ function inject() {
   for (let i = 0; i < locations.length; i++) {
 
     let locationContainer = document.createElement('div');
+    let imgContainer = document.createElement('div');
+    let textContainer = document.createElement('div');
+
     let description = document.createElement('p');
-    let price= document.createElement('p');
+    let price = document.createElement('p');
     let heading = document.createElement('h2');
     let locationImage = document.createElement('img');
-    let reserveBtn= document.createElement('button');
+    let reserveBtn = document.createElement('button');
 
-    locationContainer.appendChild(heading);
-    locationContainer.appendChild(price);
-    locationContainer.appendChild(description);
-    locationContainer.appendChild(locationImage);
-    locationContainer.appendChild(reserveBtn);
+    description.className = "description";
+    locationContainer.className = "card";
+    locationContainer.appendChild(imgContainer);
+    imgContainer.className = "divLeft";
+    locationContainer.appendChild(textContainer);
+    textContainer.className = "divRight";
+
+    imgContainer.appendChild(locationImage);
+    textContainer.appendChild(heading);
+    textContainer.appendChild(description);
+    // locationContainer.appendChild(locationImage);
+    textContainer.appendChild(price);
+    textContainer.appendChild(reserveBtn);
     result.appendChild(locationContainer);
 
-   
-    price.textContent=locations[i].price;
+
+    price.textContent = 'Price: ' + locations[i].price;
     heading.textContent = locations[i].locationName;
     description.textContent = locations[i].description;
     locationImage.src = locations[i].locationImg;
-    reserveBtn.textContent='Book Now!';//add link in array to book
-    
+    reserveBtn.textContent = 'Book Now!';//add link in array to book
+
   }
 
 
