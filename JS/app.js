57e448f939fedb0dfb9b5855a15dcd0f7266bc71
Fadi-Nayef <adraf38@hotmail.com>
2021-04-18 23:58:36 +0300

    mohammed and nazmih work merged

--- JS/app.js
+++ JS/app.js
@@ -3,22 +3,22 @@
 // }
 /////////////declare an array to add the cities/////
 
-const names =['gym1','gym2','gym3','gym4','gym4','gym5','gym6','gym7','gym8'];
-const cities = ['Amman', 'Zarqa', 'Irbid','Amman', 'Zarqa', 'Irbid','Online','Online'];
-const description = ['FirstDesqAmman', 'SecondDesqZarqa', 'ThirdDesqIrbid','2FirstDesqAmman', '2SecondDesqZarqa', '2ThirdDesqIrbid','FIRSTonline','SecondOnline'];
-const image = ['FirstimgAmman', 'SecondimgZarqa', 'ThirdimgIrbid','2FirstimgAmman', '2SecondimgZarqa', '2ThirdimgIrbid','FIRSTimgonline','SecondimgOnline'];
-const price = [50, 150, 100,120,150,200,75,250];
-const category = ['Men', 'Women', 'Men','Mix','Men','Mix','Mix','Mix'];
+const names = ['gym1', 'gym2', 'gym3', 'gym4', 'gym4', 'gym5', 'gym6', 'gym7', 'gym8'];
+const cities = ['Amman', 'Zarqa', 'Irbid', 'Amman', 'Zarqa', 'Irbid', 'Online', 'Online'];
+const description = ['FirstDesqAmman', 'SecondDesqZarqa', 'ThirdDesqIrbid', '2FirstDesqAmman', '2SecondDesqZarqa', '2ThirdDesqIrbid', 'FIRSTonline', 'SecondOnline'];
+const image = ['FirstimgAmman', 'SecondimgZarqa', 'ThirdimgIrbid', '2FirstimgAmman', '2SecondimgZarqa', '2ThirdimgIrbid', 'FIRSTimgonline', 'SecondimgOnline'];
+const price = [50, 150, 100, 120, 150, 200, 75, 250];
+const category = ['Men', 'Women', 'Men', 'Mix', 'Men', 'Mix', 'Mix', 'Mix'];
 
 ///////////////end of the cities info array//////
 
 let locations = [];
 
-function GymCategories(name,cities, description, image, price, category) {
-  this.names =name;
-  this.cities=cities;
-  this.description =description;
-  this.image =image;
+function GymCategories(name, cities, description, image, price, category) {
+  this.names = name;
+  this.cities = cities;
+  this.description = description;
+  this.image = image;
   this.price = price;
   this.category = category;
 
@@ -26,7 +26,7 @@ function GymCategories(name,cities, description, image, price, category) {
 }
 //////for loop to loop over the cities ifo arrays to insance the objects///////
 for (let i = 0; i < cities.length; i++) {
-  new GymCategories(names[i],cities[i], description[i], image[i], price[i], category[i]);
+  new GymCategories(names[i], cities[i], description[i], image[i], price[i], category[i]);
 }
 console.log(locations);
 /////////////the end of instaces/////////
@@ -35,7 +35,7 @@ console.log(locations);
 let result = document.getElementById('result');
 function inject() {
   for (let i = 0; i < locations.length; i++) {
-    
+
     let locationContainer = document.createElement('div');
     locationContainer.className = 'location';
     result.appendChild(locationContainer);
@@ -69,8 +69,21 @@ function inject() {
     description.textContent = locations[i].description;
     locationImage.src = locations[i].locationImg;
     reserveBtn.textContent = 'Book Now!'; //add link in array to book
+    // -------------------- mohamad and nazmih -------------------------
+    reserveBtn.addEventListener('click', goToCheckout);
+
+    function goToCheckout(event) {
+      event.preventDefault();
+      let stringOfLocations = JSON.stringify(locations[i]);
+      localStorage.setItem('gyms', stringOfLocations);
+
+      console.log(stringOfLocations);
+
+      window.location.href = 'checkout.html';
+    }
   }
 }
+
 
 inject();
 //--------------------------------------------------------------------------------------------
@@ -87,25 +100,27 @@ function handleCustomerSubmit(event) {
 
   let genderCategouries = event.target.categouryGender.value;
 
-  let onlineCourse='';
-  if(event.target.online.checked){
-    onlineCourse='Online';
+  let onlineCourse = '';
+  if (event.target.online.checked) {
+    onlineCourse = 'Online';
     // console.log(onlineCourse);
   }
-  else{
-     onlineCourse = '';
+  else {
+    onlineCourse = '';
   }
 
   //  console.log('onlinehere',onlineCourse);
-console.log(place,genderCategouries);
-// ----------------------------for loop to loop over the user choices FADI&MOHAMMED--------------------
+  console.log(place, genderCategouries);
+  // ----------------------------for loop to loop over the user choices FADI&MOHAMMED--------------------
   for (let i = 0; i < locations.length; i++) {
-if (locations[i].cities == onlineCourse){
- console.log('first',locations[i]);
-}
-  if (((locations[i].category == genderCategouries) && (genderCategouries!=='NoCategoury'))|| ((locations[i].cities==place) && (place !=='NoPlaceCategoury') )){
-console.log('secondelse',locations[i]);
+    if (locations[i].cities == onlineCourse) {
+      console.log('first', locations[i]);
+    }
+    if (((locations[i].category == genderCategouries) && (genderCategouries !== 'NoCategoury')) || ((locations[i].cities == place) && (place !== 'NoPlaceCategoury'))) {
+      console.log('secondelse', locations[i]);
 
- }}}
+    }
+  }
+}
  //REMOVE EVENT LISTENER
 // // -------------------------------------------------------------THE END OF for loop to loop over the user choices FADI&MOHAMMED------------
