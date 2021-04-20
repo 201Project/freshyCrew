21851cb97551aecc10dbffb351ef9c36fd3974ea
Alia999 <aliaahmad1998a@gmail.com>
2021-04-20 17:25:05 +0300

    content

--- JS/calculating.js
+++ JS/calculating.js
@@ -139,9 +139,10 @@ window.addEventListener("load", start);
 
 //------------------------------ start render data for div---------------------
 let arrTips = [];
-function advice(defintion, source) {
+function advice(titlCase,defintion,titlUl, source) {
+    this.titlCase=titlCase;
     this.defintion = defintion;
-
+    this.titlUl=titlUl;
     this.source = source;
 
     arrTips.push(this);
@@ -150,17 +151,31 @@ function advice(defintion, source) {
 }
 
 
-let underWeight = new advice('lorem underWeight', '//players.brightcove.net/293884104/gh5LeNtQaQ_default/index.html?videoId=5443859718001');
-let normalWeight = new advice('lorem normalWeight', '//players.brightcove.net/293884104/gh5LeNtQaQ_default/index.html?videoId=5443859718001');
-let overWeight = new advice('lorem overWeight', '//players.brightcove.net/293884104/gh5LeNtQaQ_default/index.html?videoId=5443859718001');
+let underWeight = new advice('What is a good way to gain weight for  underweight people?','Being underweight can be a concern if it is the result of poor nutrition or if you are pregnant or have other health concerns. So, if you are underweight, see your doctor or dietitian for an evaluation. Together ,you can plan how to meet your goal weight.','Here are some healthy ways to gain weight when you are underweight:', 'https://www.youtube.com/embed/KM3ko1Z4amA');
+let normalWeight = new advice('To keep your weight as it is:',' A healthy weight, or normal weight, means that your Body Mass Index falls within a weight range that is not associated with an increased risk for weight-related diseases and health issues. Body Mass Index is determined by your height and your weight.','Here are some healthy ways to keep your weight as it is:', 'https://www.youtube.com/embed/iVdlEWXWXRg');
+let overWeight = new advice('What is a good way to  lose weight?','Being overweight or fat is having more body fat than is optimally healthy.Being overweight is especially common where food supplies are plentiful and lifestyles are sedentary. As of 2003, excess weight reached epidemic proportions globally, with more than 1 billion adults being either overweight or obese.' ,' Here are some healthy ways to lose weight when you are overweight: ', 'https://www.youtube.com/embed/GGP3NbP7O2w');
 
-let tipsUnderWeight = ['A', 'B', 'C'];
-let tipsNormalWeight = ['1', '2', '3'];
-let tipsOverWeight = ['@', '%', '~'];
+let tipsUnderWeight = ['Eat more frequently.', 'Choose nutrient-rich foods.','Try smoothies and shakes.','Watch when you drink.','Make every bite count.',
+'Exercise.'];
+/*********** */
+let tipsNormalWeight = ['Exercise Often.','Try Eating Breakfast Every Day.','Eat Lots of Protein.','Weigh Yourself Regularly.','Be Mindful of Your Carb Intake.','Lift Weights.'];
+/*************** */
+let tipsOverWeight = ['Do not skip breakfast.','Eat regular meals.','Eat plenty of fruit and veg.','Get more active.','Drink plenty of water.','Eat high fibre foods.'];
 
 advice.prototype.renderObjects = function () {
     // let parent = document.getElementById('addData') ;
     // parent = document.getElementById("addData").style.visibility = "visible";
+
+
+
+
+let titlCaseEL=document.createElement('h2');
+    parent.appendChild(titlCaseEL);
+    titlCaseEL.textContent=this.titlCase;
+
+
+
+
     let def = document.createElement('p');
     parent.appendChild(def);
     def.textContent = this.defintion;
@@ -174,7 +189,7 @@ advice.prototype.renderObjects = function () {
 
 
 
-    let apperVideo = document.createElement('IFRAME')
+    let apperVideo = document.createElement('IFRAME');
 
     apperVideo.setAttribute('src', this.source);
     apperVideo.setAttribute("height", "540");
@@ -199,7 +214,11 @@ advice.prototype.renderObjects = function () {
 
 
 
-
+     let ULTitleEl=document.createElement('h3');
+     parent.appendChild(ULTitleEl)
+     ULTitleEl.textContent=this.titlUl;
+     console.log(this.titlUl);
+     console.log(ULTitleEl);
 
 
 
@@ -210,7 +229,13 @@ advice.prototype.renderObjects = function () {
 //-------------------------------------------- End render data for div-------------
 function listTips(arrList) {
 
+
+
+    
+
+
     let UlElement = document.createElement('ul');
+
 
     for (let i = 0; i < arrList.length; i++) {
 
