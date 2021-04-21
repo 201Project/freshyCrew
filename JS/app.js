34ca091de7543a263ee250db099a1fb3526e17a7
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-04-21 05:44:38 +0300

    laslast fix needed (I hope)

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,28 @@
 'use strict';
 let locations = [];
+let clickedItem = [];
+let cart = document.getElementById('cart');
+let icon = document.createElement('i');
+function cartIcon() {
 
-let result = document.getElementById('result');
+  cart.appendChild(icon);
+  icon.textContent = '(' + localStorage.getItem('cartCounter') + ')';
+  icon.className = 'fa fa-shopping-cart';
+  icon.style.fontSize = '24px';
+  cart.style.display = 'inline-block';
+}
 
+if (localStorage.getItem('gyms') !== null) {
+  let lc = localStorage.getItem('gyms');
+  let parsedStorage = JSON.parse(lc);
+  clickedItem = parsedStorage;
+}
 
+let counter=0;
+if(localStorage.getItem('cartCounter')){
+  counter=localStorage.getItem('cartCounter');
+  cartIcon();
+}
 const gymName = ['gym1', 'gym2', 'gym3', 'gym4', 'gym5', 'gym6', 'gym7', 'gym8'];
 const gymLocation = ['Amman', 'Zarqa', 'Irbid', 'Amman', 'Zarqa', 'Irbid', 'Online', 'Online'];
 const gymDesq = ['FirstDesqAmman', 'SecondDesqZarqa', 'ThirdDesqIrbid', '2FirstDesqAmman', '2SecondDesqZarqa', '2ThirdDesqIrbid', 'FIRSTonline', 'SecondOnline'];
@@ -22,87 +41,67 @@ function GymCategories(name, cities, description, image, price, gender) {
   locations.push(this);
 }
 for (let i = 0; i < gymLocation.length; i++) {
-new GymCategories(gymName[i], gymLocation[i], gymDesq[i], gymImg[i], gymCoast[i], gymGender[i]);
+  new GymCategories(gymName[i], gymLocation[i], gymDesq[i], gymImg[i], gymCoast[i], gymGender[i]);
 
 }
- let clickedItem=[];
-if (localStorage.getItem('gyms') !== null) {
-   let parsedStorage = JSON.parse(localStorage.getItem('gyms'));
-  clickedItem = parsedStorage;
-}
-
-GymCategories.prototype.inject =function(gym){
 
 
-    let locationContainer = document.createElement('div');
-    locationContainer.className = 'location';
-    result.appendChild(locationContainer);
+GymCategories.prototype.inject = function (gym) {
 
-    let imgContainer = document.createElement('div');
-    imgContainer.className = 'divLeft';
-    locationContainer.appendChild(imgContainer);
 
-    let locationImage = document.createElement('img');
-    imgContainer.appendChild(locationImage);
-    locationImage.src = gym.gymImg;
+  let locationContainer = document.createElement('div');
+  locationContainer.className = 'location';
+  result.appendChild(locationContainer);
 
-    let textContainer = document.createElement('div');
-    textContainer.className = 'divRight';
-    locationContainer.appendChild(textContainer);
+  let imgContainer = document.createElement('div');
+  imgContainer.className = 'divLeft';
+  locationContainer.appendChild(imgContainer);
 
-    let heading = document.createElement('h2');
-    textContainer.appendChild(heading);
-    heading.textContent = gym.gymName;
+  let locationImage = document.createElement('img');
+  imgContainer.appendChild(locationImage);
+  locationImage.src = gym.gymImg;
 
-    let description = document.createElement('p');
-    description.className = 'description';
-    textContainer.appendChild(description);
-    description.textContent =gym.gymDesq;
+  let textContainer = document.createElement('div');
+  textContainer.className = 'divRight';
+  locationContainer.appendChild(textContainer);
 
-    let price = document.createElement('p');
-    textContainer.appendChild(price);
-    price.textContent = 'Price: ' + gym.gymCoast;
+  let heading = document.createElement('h2');
+  textContainer.appendChild(heading);
+  heading.textContent = gym.gymName;
 
-    let reserveBtn = document.createElement('button');
-    textContainer.appendChild(reserveBtn);
+  let description = document.createElement('p');
+  description.className = 'description';
+  textContainer.appendChild(description);
+  description.textContent = gym.gymDesq;
 
-    // console.log(reserveBtn);
-    reserveBtn.textContent = 'Book Now!'; //add link in array to book
-// console.log(this);
+  let price = document.createElement('p');
+  textContainer.appendChild(price);
+  price.textContent = 'Price: ' + gym.gymCoast;
 
-    reserveBtn.addEventListener('click', goToCheckout);
+  let reserveBtn = document.createElement('button');
+  textContainer.appendChild(reserveBtn);
 
-    function goToCheckout(event) {
-      event.preventDefault();
-      let count = 0;
-     console.log(gym);
+  reserveBtn.textContent = 'Book Now!';
 
-            if(!(clickedItem.includes(gym)))
-            {
-              alert('added!');
-              clickedItem.push(gym);
-              let stringOfLocations = JSON.stringify(clickedItem);
-              localStorage.setItem('gyms', stringOfLocations);
-              if(localStorage.getItem('cartCounter')){
-                let paresedNumber=parseInt(localStorage.getItem('cartCounter'));
-                localStorage.setItem('cartCounter',paresedNumber+1);
-              }
-              
-              else
-              {
-                count++;
-                localStorage.setItem('cartCounter',count);
-              }
-              let counter=document.getElementById('cart');
-              counter.textContent=localStorage.getItem('cartCounter');
-            }
-            else{
-              alert('already there');
-            }
-          }
+
+  reserveBtn.addEventListener('click', goToCheckout);
+  function goToCheckout() {
+
+    if (!clickedItem.includes(gym)) {
+      clickedItem.push(gym);
+      console.log('unique');
+      let str = JSON.stringify(clickedItem);
+      localStorage.setItem('gyms', str);
+      counter++;
+      localStorage.setItem('cartCounter',counter);
+      cartIcon();
+    }
+
+  }
 
 }
-for(let i=0;i<locations.length;i++){
+
+for (let i = 0; i < locations.length; i++) {
   GymCategories.prototype.inject(locations[i]);
 }
 
@@ -129,35 +128,32 @@ function handleCustomerSubmit(event) {
     OnlineCourse = '';
   }
 
+  result.innerHTML = "";
+
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].gymLocation == OnlineCourse) {
+
+      GymCategories.prototype.inject(locations[i]);
+
+      console.log('first', GymCategories);
 
 
-
-
-    result.innerHTML="";
-
-for (let i = 0; i < locations.length; i++) {
-  if (locations[i].gymLocation == OnlineCourse) {
-
-    GymCategories.prototype.inject(locations[i]);
-
-    console.log('first',GymCategories);
-
-
-    }  if ((locations[i].gymGender == genderCategouries) && (locations[i].gymLocation == place)) {
+    } if ((locations[i].gymGender == genderCategouries) && (locations[i].gymLocation == place)) {
       GymCategories.prototype.inject(locations[i]);
 
       // console.log('secondelse', locations[i]);
 
 
-    // }else if ((locations[i].gymGender == genderCategouries) && (genderCategouries !== 'NoCategoury')){
+      // }else if ((locations[i].gymGender == genderCategouries) && (genderCategouries !== 'NoCategoury')){
 
-    //   console.log('third', locations[i]);
+      //   console.log('third', locations[i]);
 
-    // } else if  ((locations[i].gymLocation == place) && (place !== 'NoPlaceCategoury')){
-    //   console.log('fourth', locations[i]);
+      // } else if  ((locations[i].gymLocation == place) && (place !== 'NoPlaceCategoury')){
+      //   console.log('fourth', locations[i]);
 
+    }
   }
-}
 
 
 }
+
--- JS/checkoutapp.js
+++ JS/checkoutapp.js
@@ -1,57 +1,66 @@
 'use strict';
 //-------------start form function for the checkout page By Alaa & Zainb---------------------------------------------------//
+
 let details = document.getElementById('detailsCheckout');
 let formCheckout = document.getElementById('formCheckout');
 formCheckout.addEventListener('submit', handleSaveCheckout);
-let cartCounter=localStorage.getItem('cartCounter');
-let counter=document.getElementById('cart');
-let cancelBtn=document.getElementById('cancel');
-let emptytable=document.getElementById('emptytable');
+let cartCounter = localStorage.getItem('cartCounter');
+let counter = document.getElementById('cart');
+let cancelBtn = document.getElementById('cancel');
+let emptytable = document.getElementById('emptytable');
 let stringToArrayData;
 let table = document.createElement('table');
 let reservedGym = document.getElementById('reservedGym');
-if(cartCounter>0)
-{
-  counter.textContent=localStorage.getItem('cartCounter');
-  emptytable.style.display='none';
+let majorTotal = 0;
+let cart = document.getElementById('cart');
+let icon = document.createElement('i');
+
+function cartIcon() {
+  
+  cart.appendChild(icon);
+  icon.textContent = '(' + localStorage.getItem('cartCounter') + ')';
+  icon.className = 'fa fa-shopping-cart';
+  icon.style.fontSize = '24px';
+  cart.style.display='inline-block';
 }
-else{
-  counter.textContent='';
+
+
+
+if (cartCounter > 0) {
+  emptytable.style.display = 'none';
+}
+else {
+  counter.textContent = '';
   localStorage.removeItem('gyms');
   localStorage.removeItem('cartCounter');
-  emptytable.style.display='block';
+  emptytable.style.display = 'block';
 }
 
 
 function handleSaveCheckout(e) {
   e.preventDefault();
-  
+
   let userName = e.target.userName.value;
-  alert('Thanks from buying from us!, '+userName);
-  let nameInput=document.getElementById('userName');
-  nameInput.value='';
-  let email=document.getElementById('email');
-  email.value='';
-  let phone=document.getElementById('phone');
-  phone.value='';
-  let address=document.getElementById('address');
-  address.value='';
-  
+  alert('Thanks from buying from us!, ' + userName);
+  let nameInput = document.getElementById('userName');
+  nameInput.value = '';
+  let email = document.getElementById('email');
+  email.value = '';
+  let phone = document.getElementById('phone');
+  phone.value = '';
+  let address = document.getElementById('address');
+  address.value = '';
+  emptyContent();
+
 }
 
-cancelBtn.addEventListener('click',cancelOrder);
-function cancelOrder(){
-  let confirmMsg=confirm("are you sure you want to remove all the items from the cart?");
-  if(confirmMsg)
-  {
-    localStorage.removeItem('gyms');
-    localStorage.removeItem('cartCounter');
-    reservedGym.innerHTML='';
-    table.innerHTML='';
-    counter.textContent='';
-    emptytable.style.display='block';
+cancelBtn.addEventListener('click', cancelOrder);
+function cancelOrder() {
+  let confirmMsg = confirm("are you sure you want to remove all the items from the cart?");
+  if (confirmMsg) {
+    emptyContent();
   }
-  else{
+  else {
     alert('Canceled!');
   }
 
@@ -78,27 +87,28 @@ function getFromStorage() {
   let data = localStorage.getItem('gyms');
 
   stringToArrayData = JSON.parse(data);
-    if(cartCounter>0){
-   //hide the empty cart message
-    emptytable.style.display='none';
+  if (cartCounter > 0) {
+    cartIcon();
+    //hide the empty cart message
+    emptytable.style.display = 'none';
     for (let i = 0; i < stringToArrayData.length; i++) {
 
       let tr = document.createElement('tr');
       table.appendChild(tr);
-  
+
       let imageTd = document.createElement('td');
       tr.appendChild(imageTd);
       let locationImage = document.createElement('img');
       // reservedGym.appendChild(locationImage);
       locationImage.src = stringToArrayData[i].gymImg;
-    
+
       imageTd.appendChild(locationImage);
-  
+
       let nameTd = document.createElement('td');
       tr.appendChild(nameTd);
       nameTd.textContent = stringToArrayData[i].gymName;
-  
-  
+
+
       let reserveNumberTd = document.createElement('td');
       let reserveNumber = document.createElement('input');
       reserveNumber.type = 'number';
@@ -107,58 +117,54 @@ function getFromStorage() {
       reserveNumber.max = 12;
       tr.appendChild(reserveNumberTd);
       reserveNumberTd.appendChild(reserveNumber);
-  
+
       let priceTd = document.createElement('td');
       tr.appendChild(priceTd);
       priceTd.textContent = stringToArrayData[i].gymCoast;
-  
+
       let totalTd = document.createElement('td');
       tr.appendChild(totalTd);
       totalTd.textContent = stringToArrayData[i].gymCoast;
-  
+
       reserveNumber.addEventListener('change', calculateTotal);
       function calculateTotal() {
         totalTd.textContent = stringToArrayData[i].gymCoast * reserveNumber.value;
       }
-      let clearTd=document.createElement('td');
+      let clearTd = document.createElement('td');
       tr.appendChild(clearTd);
-      let clearIcon=document.createElement('i');
-      let clearBtn=document.createElement('button');
-      
-      clearBtn.style.fontSize='24px';
-      clearIcon.className='fas fa-trash-alt';
+      let clearBtn = document.createElement('button');
       clearTd.appendChild(clearBtn);
-      clearBtn.appendChild(clearIcon);
-      clearBtn.textContent='clear';
-      clearBtn.addEventListener('click',clearItem);
-      //<button style='font-size:24px'><i class='fas fa-trash-alt'></i></button>
-      function clearItem(){
-        stringToArrayData[i]=stringToArrayData[stringToArrayData.length-1];
+      clearBtn.textContent = 'clear';
+      clearBtn.addEventListener('click', clearItem);
+      function clearItem() {
+        stringToArrayData[i] = stringToArrayData[stringToArrayData.length - 1];
         stringToArrayData.pop();
-        localStorage.setItem('gyms',JSON.stringify(stringToArrayData));
-        tr.innerHTML="";
+        localStorage.setItem('gyms', JSON.stringify(stringToArrayData));
+        tr.innerHTML = "";
         cartCounter--;
-        localStorage.setItem('cartCounter',cartCounter);
-        if(cartCounter>0){
-          counter.textContent=localStorage.getItem('cartCounter');
-
+        localStorage.setItem('cartCounter', cartCounter);
+        if (cartCounter > 0) {
+          cartIcon();
         }
-        else{
+        else {
           emptyContent();
         }
       }
     }
   }
-  else
-  {
+  else {
     emptyContent();
   }
 }
-  getFromStorage();
-  
-
-function emptyContent(){
-  reservedGym.innerHTML='';
-  table.innerHTML='';
-  emptytable.style.display='block';
+getFromStorage();
+function emptyContent() {
+  localStorage.removeItem('gyms');
+  localStorage.removeItem('cartCounter');
+  reservedGym.innerHTML = '';
+  table.innerHTML = '';
+  emptytable.style.display = 'block';
+  cart.style.display='none';
 }
+
+
+
