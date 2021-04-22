bd45943e36c7c4043226349ad45c4d4dd9f1a984
Alia999 <aliaahmad1998a@gmail.com>
2021-04-22 11:18:04 +0300

    fixed

--- JS/checkoutapp.js
+++ JS/checkoutapp.js
@@ -1,6 +1,5 @@
 'use strict';
 //-------------start form function for the checkout page By Alaa & Zainb---------------------------------------------------//
-
 let formCheckout = document.getElementById('formCheckout');
 formCheckout.addEventListener('submit-info', handleSaveCheckout);
 let cartCounter = localStorage.getItem('cartCounter');
@@ -16,16 +15,12 @@ let cart = document.getElementById('cart');
 let icon = document.createElement('i');
 let totaldiv=document.getElementById('total');
 function cartIcon() {
-
   cart.appendChild(icon);
   icon.textContent = '(' + localStorage.getItem('cartCounter') + ')';
   icon.className = 'fa fa-shopping-cart';
   icon.style.fontSize = '24px';
   cart.style.display='inline-block';
 }
-
-
-
 if (cartCounter > 0) {
   emptytable.style.display = 'none';
   formCheckout.style.display='inline-block';
@@ -37,11 +32,8 @@ else {
   emptytable.style.display = 'block';
   formCheckout.style.display='none';
 }
-
-
 function handleSaveCheckout(e) {
   e.preventDefault();
-
   let userName = e.target.userName.value;
   alert('Thanks from buying from us!, ' + userName);
   let nameInput = document.getElementById('userName');
@@ -53,9 +45,7 @@ function handleSaveCheckout(e) {
   let address = document.getElementById('address');
   address.value = '';
   emptyContent();
-
 }
-
 cancelBtn.addEventListener('click', cancelOrder);
 function cancelOrder() {
   let confirmMsg = confirm("are you sure you want to remove all the items from the cart?");
@@ -65,14 +55,9 @@ function cancelOrder() {
   else {
     alert('Canceled!');
   }
-
 }
 //--------------------------------------------the end of form function for the checkout page By Alaa & Zainb--------------------//
-
 //------------------------------mohamad and nazmih-----------------
-
-
-
 function getFromStorage() {
   let headingsArr = ['Image', 'Name', 'Months', 'Price', 'Total', 'Clear item'];
   reservedGym.appendChild(table);
@@ -83,9 +68,7 @@ function getFromStorage() {
     trHeadings.appendChild(tHead);
     tHead.textContent = headingsArr[i];
   }
-
   let data = localStorage.getItem('gyms');
-
   stringToArrayData = JSON.parse(data);
   if (cartCounter > 0) {
     cartIcon();
@@ -100,7 +83,6 @@ function getFromStorage() {
       let locationImage = document.createElement('img');
       // reservedGym.appendChild(locationImage);
       locationImage.src = stringToArrayData[i].gymImg;
-      
       imageTd.appendChild(locationImage);
       
       let nameTd = document.createElement('td');
@@ -117,35 +99,47 @@ function getFromStorage() {
       tr.appendChild(reserveNumberTd);
       reserveNumberTd.appendChild(reserveNumber);
       
-      let priceTd = document.createElement('td');
-      tr.appendChild(priceTd);
-      priceTd.textContent = stringToArrayData[i].gymCoast;
-      
-      let totalTd = document.createElement('td');
-      tr.appendChild(totalTd);
-      totalTd.textContent = stringToArrayData[i].gymCoast;
+  
+        
+        let priceTd = document.createElement('td');
+        priceTd.setAttribute('id','firstPrice')
+        tr.appendChild(priceTd);
+        let one = priceTd.textContent = stringToArrayData[i].gymCoast;
+        console.log("one", one);
+  
+        let totalTd = document.createElement('td');
+        tr.appendChild(totalTd);
+        let tow = totalTd.textContent= stringToArrayData[i].gymCoast;
+        console.log("tow", tow);
       
       
       // reserveNumber.addEventListener('change', calculateTotal);
-      // // let priceArr=[];
-      // // console.log(productPrice++, 'dana');            
+      // let priceArr=[];
       
       // function calculateTotal() {
-      // //   for (let j = 0; j < stringToArrayData[i].gymCoast.length; j++) {
-      // //    let loopingPrice= priceArr[j].push(productPrice);
-      // //   console.log('dana1',loopingPrice);
-      // //   console.log('daana3',productPrice);
-      //   // }
+      //   for (let j = 0; j < stringToArrayData[i].gymCoast.length; j++) {
+      //     let loopingPrice= priceArr[j].push(productPrice);
+      //     console.log('dana1',loopingPrice);
+      //     console.log('daana3',productPrice);
+      //     console.log(productPrice, 'dana');            
+      //   }
       //   total = stringToArrayData[i].gymCoast * reserveNumber.value;
       //   totalTd.textContent = total;
       //   totaldiv.textContent=majorTotal;
-      //   console.log('fira', stringToArrayData[i].gymCoast * reserveNumber.value);
+      //   console.log('Totalsssss', stringToArrayData[i].gymCoast * reserveNumber.value);
       // }
+      let four;
       reserveNumber.addEventListener('change', calculateTotal);
+      // let Arr = [];
       function calculateTotal() {
         total=stringToArrayData[i].gymCoast * reserveNumber.value;
-        totalTd.textContent = total;
-        totaldiv.textContent=majorTotal;
+        four = totalTd.textContent = total;
+        console.log("four", four);
+        // Arr.push(Number(four.length-1));
+        let three = totaldiv.textContent=majorTotal+four;
+        console.log("three", three);
+        console.log("Arr", Arr);
+        // majorTotal+=three;
       }
       let clearTd = document.createElement('td');
       tr.appendChild(clearTd);
@@ -167,15 +161,12 @@ function getFromStorage() {
           emptyContent();
         }
       }
-
     }
-
   }
   else {
     emptyContent();
   }
 }
-
 getFromStorage();
 function emptyContent() {
   localStorage.removeItem('gyms');
@@ -186,6 +177,10 @@ function emptyContent() {
   cart.style.display='none';
   formCheckout.style.display='none';
 }
+
+
+
+
 
 
 
